49c0c98be11530be0aab724fe9f909fd6410c335
seralv <sergioalvaro.14@gmail.com>
2024-08-21 22:23:33 -0400

    UI finished

--- js/script.js
+++ js/script.js
@@ -2,22 +2,31 @@ const container = document.querySelector(".grid-container");
 const boardChoice = document.querySelector("#boardChoice");
 const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", () => {
-  console.log(boardChoice.value);
-});
+function handleClick() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-for (let i = 1; i <= 16; i++) {
-  const div = document.createElement("div");
-  // div.textContent = `${i}`;
-  div.style.margin = "1px";
-  div.style.height = "100px";
-  div.style.width = "100px";
-  div.style.background = "LightBlue";
-  div.style.textAlign = "center";
-  div.style.lineHeight = "100px";
-  container.appendChild(div);
-  div.addEventListener("mouseover", () => {
-    div.style.background = "red";
-    div.style.color = "white";
-  });
+  if (boardChoice.value < 101 && boardChoice.value > 0) {
+    const boxes = boardChoice.value ** 2;
+    const sizeDiv = Math.floor(100 / boardChoice.value);
+    for (let i = 1; i <= boxes; i++) {
+      const div = document.createElement("div");
+      div.style.margin = "1px";
+      div.style.height = `${sizeDiv}vh`;
+      div.style.width = `${sizeDiv}vw`;
+      div.style.background = "LightBlue";
+      div.style.textAlign = "center";
+      div.style.lineHeight = "100px";
+      container.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        div.style.background = "red";
+        div.style.color = "white";
+      });
+    }
+  } else {
+    alert("Please enter a number between 1 to 100");
+  }
 }
+
+btn.addEventListener("click", handleClick);
